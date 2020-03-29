95abf2d4eac186ca3715ca6f56492149995646b7
golden <41384430+remagnedlogeht@users.noreply.github.com>
2020-03-30 01:11:59 +0300

    Create attachments.py

---
+++ cogs/attachments.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+class attachments(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        channel = self.bot.get_channel(690584660123582494)
+        if not message.channel.category_id:
+            return
+        if not message.channel.category_id == 553232325832343574:
+            return
+        if message.attachments:
+            for i in message.attachments:
+                embed = discord.Embed(title="shitty shit")
+                embed.set_image(url=i.url)
+                await channel.send(embed=embed)
+        else:
+            return
+
+def setup(bot):
+    bot.add_cog(attachments(bot))
